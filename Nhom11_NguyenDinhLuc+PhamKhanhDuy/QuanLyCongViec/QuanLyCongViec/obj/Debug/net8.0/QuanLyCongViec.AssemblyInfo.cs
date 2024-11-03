7fe3cedf0af4a8fa1c997e854c5f32282147cfd2
1uccc <1ucclucnd0201@gmail.com>
2024-11-03 22:33:14 +0700

    done

--- Nhom11_NguyenDinhLuc+PhamKhanhDuy/QuanLyCongViec/QuanLyCongViec/obj/Debug/net8.0/QuanLyCongViec.AssemblyInfo.cs
+++ Nhom11_NguyenDinhLuc+PhamKhanhDuy/QuanLyCongViec/QuanLyCongViec/obj/Debug/net8.0/QuanLyCongViec.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyCongViec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8c90994724090d97b43442a3ceee35384869dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyCongViec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyCongViec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
